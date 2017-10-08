88e264d9b5c0aab794090da6385bbfc34a5ff1ed
Phillip Kuznetsov <philkuz@berkeley.edu>
2017-10-07 22:31:21 -0700

    Adding deepdream

---
+++ download_net.py
@@ -0,0 +1,28 @@
+import urllib.request
+import os
+import zipfile
+
+def download_net(data_dir):
+    """
+    Downloads inceptionh5 to data_dir.
+    InceptionH5 is a modficiation of Inception-v1 specifically
+    for use with Image processing techniques since it allows
+    you to input an image of arbitrary width/height
+    """
+    url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    model_name = os.path.split(url)[-1]
+    local_zip_file = os.path.join(data_dir, model_name)
+
+    if not os.path.exists(local_zip_file):
+        # Download
+        model_url = urllib.request.urlopen(url)
+        with open(local_zip_file, 'wb') as output:
+            output.write(model_url.read())
+
+        # Extract
+        with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)
